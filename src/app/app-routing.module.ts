93dfe3b86881653f8c957ff86aa6b1af348f5a9d
Ossama Rafique <contact@ossamarafique.com>
2023-03-07 16:55:23 -0800

    Added the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterDetailComponent } from './pages/character/character-detail/character-detail.component';
+import { MovieDetailComponent } from './pages/movie/movie-detail/movie-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/movie/movie.module').then((m) => m.MovieModule),
+  },
+  {
+    path: 'character',
+    loadChildren: () =>
+      import('./pages/character/character.module').then(
+        (m) => m.CharacterModule
+      ),
+  },
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { AppState } from './shared/store/app.state';
+import { setLoadingSpinner } from './shared/store/shared/shared.actions';
+import { getLoading } from './shared/store/shared/shared.selector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'drooms-star-wars-application';
+export class AppComponent implements OnInit, OnDestroy {
+  private _destroy$: Subject<void> = new Subject<void>();
+  constructor(
+    private spinner: NgxSpinnerService,
+    private store: Store<AppState>
+  ) {}
+
+  ngOnInit(): void {
+    this.initLoadingStream();
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  private initLoadingStream(): void {
+    this.store
+      .select(getLoading)
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((show) => {
+        if (show) this.spinner.show();
+        else this.spinner.hide();
+      });
+  }
 }
